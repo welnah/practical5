d12f98b97e3a7d0b3f63a33259419708b54aeda1
Welnah <pontshokoopedi@gmail.com>
2022-04-10 20:46:15 +0200

    HI

---
+++ 22735233_Practical5/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace _22735233_Practical5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.radDocOne = new System.Windows.Forms.RadioButton();
+            this.radDoCTwo = new System.Windows.Forms.RadioButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.nudAge = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a date:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(151, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Surname:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(158, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Age:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(151, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Select a doctor";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(151, 50);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(255, 23);
+            this.dateTimePicker1.TabIndex = 5;
+            this.dateTimePicker1.Value = new System.DateTime(2022, 4, 10, 12, 19, 52, 0);
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(296, 89);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(110, 23);
+            this.txtFirstName.TabIndex = 6;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(296, 118);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(110, 23);
+            this.txtSurname.TabIndex = 7;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(151, 294);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(255, 23);
+            this.btnSubmit.TabIndex = 8;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // radDocOne
+            // 
+            this.radDocOne.AutoSize = true;
+            this.radDocOne.Location = new System.Drawing.Point(151, 239);
+            this.radDocOne.Name = "radDocOne";
+            this.radDocOne.Size = new System.Drawing.Size(86, 19);
+            this.radDocOne.TabIndex = 9;
+            this.radDocOne.TabStop = true;
+            this.radDocOne.Text = "Doctor One";
+            this.radDocOne.UseVisualStyleBackColor = true;
+            this.radDocOne.CheckedChanged += new System.EventHandler(this.radDocOne_CheckedChanged);
+            // 
+            // radDoCTwo
+            // 
+            this.radDoCTwo.AutoSize = true;
+            this.radDoCTwo.Location = new System.Drawing.Point(312, 239);
+            this.radDoCTwo.Name = "radDoCTwo";
+            this.radDoCTwo.Size = new System.Drawing.Size(85, 19);
+            this.radDoCTwo.TabIndex = 10;
+            this.radDoCTwo.TabStop = true;
+            this.radDoCTwo.Text = "Doctor Two";
+            this.radDoCTwo.UseVisualStyleBackColor = true;
+            this.radDoCTwo.CheckedChanged += new System.EventHandler(this.radDoCTwo_CheckedChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(459, 59);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(329, 259);
+            this.listBox1.TabIndex = 11;
+            // 
+            // nudAge
+            // 
+            this.nudAge.Location = new System.Drawing.Point(296, 153);
+            this.nudAge.Name = "nudAge";
+            this.nudAge.Size = new System.Drawing.Size(110, 23);
+            this.nudAge.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nudAge);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.radDoCTwo);
+            this.Controls.Add(this.radDocOne);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudAge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.RadioButton radDocOne;
+        private System.Windows.Forms.RadioButton radDoCTwo;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown nudAge;
+    }
+}
+
---
+++ 22735233_Practical5/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _22735233_Practical5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Variables
+        string name, surname, date;
+        int age, price;
+        
+
+        private void radDocOne_CheckedChanged(object sender, EventArgs e)
+        {
+
+            name = Convert.ToString(txtFirstName.Text);
+            surname = Convert.ToString(txtSurname.Text);
+            age = Convert.ToInt32(nudAge.Value);
+            date = Convert.ToString(dateTimePicker1.Text);
+
+
+        }
+
+        private void radDoCTwo_CheckedChanged(object sender, EventArgs e)
+        {
+            //input
+            name = Convert.ToString(txtFirstName.Text);
+            surname = Convert.ToString(txtSurname.Text);
+            age = Convert.ToInt32(nudAge.Value);
+            date = Convert.ToString(dateTimePicker1.Text);
+
+
+        }
+
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //output
+            if (radDocOne.Checked)
+            {
+
+
+
+                if (age < 12)
+                {
+                    price = 100;
+                }
+                else if (age > 13 && age < 17)
+                {
+                    price = 120;
+                }
+                else if (age > 18 && age < 64)
+                {
+                    price = 150;
+                }
+                else if (age > 65)
+                {
+                    price = 110;
+                }
+
+
+            }
+
+            //output
+            if (radDoCTwo.Checked)
+            {
+
+
+
+                if (age < 12)
+                {
+                    price = 100;
+                }
+                else if (age > 13 && age < 17)
+                {
+                    price = 120;
+                }
+                else if (age > 18 && age < 64)
+                {
+                    price = 150;
+                }
+                else if (age > 65)
+                {
+                    price = 110;
+                }
+
+
+            }
+            listBox1.Items.Add("Appointment is on: " + date + "\n");
+            listBox1.Items.Add("Name:" + name + "\n");
+            listBox1.Items.Add("Surname:" + surname + "\n");
+            listBox1.Items.Add("Age:" + Convert.ToString(age));
+            listBox1.Items.Add("\n" + "Price: R" + price);
+        }
+        
+    }
+}
